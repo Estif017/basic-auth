4b1eabf93bde63a439962292dbe39f48b20fdbc6
Estif017 <estifoteklearegay@gmail.com>
2020-11-17 03:57:37 -0800

    finishing up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import app from './firebase'
+import { Container } from 'react-bootstrap'
+import Switch from 'react-bootstrap/esm/Switch'
+import {BrowserRouter as Router, Route } from 'react-router-dom'
+import PrivateRoutes from './components/PrivateRoutes'
+import { AuthProvider } from './context/AuthContext'
+import Dashboard from './pages/Dashboard'
+import ForgetPasword from './pages/ForgetPasword'
+import LogIn from './pages/LogIn'
+import SignUp from './pages/SignUp'
+import UpdateProfile from './pages/UpdateProfile'
+// import app from './firebase'
 
 export default function App() {
-  console.log(app)
+  
   return (
-    <div>
-      Basic Auth React
-    </div>
+    <Container 
+      className='d-flex align-items-center justify-content-center'
+      style={{minHeight:"100vh"}}>
+        <div className="w-100" style={{maxWidth:'400px'}}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <PrivateRoutes exact path='/' component={Dashboard}/>
+                <PrivateRoutes path='/update-profile' component={UpdateProfile}/>
+                <Route path='/signup' component={SignUp}/>
+                <Route path='/login' component={LogIn}/>
+                <Route path='/forgot-password' component={ForgetPasword}/>
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+    </Container>
   )
 }
